e4150c444cd43db18d5bb429cd31e62d0163555b
guishan <guishan@dian.so>
2018-11-07 16:17:35 +0800

    add the base62 method

---
+++ base62/base62_test.go
@@ -0,0 +1,18 @@
+package base62
+/*
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPadding(t *testing.T)  {
+	b := []byte{1,2,3}
+	padding(&b)
+	fmt.Println(b)
+}
+
+func TestBase62Encode(t *testing.T) {
+	b := []byte{1,2,3}
+	fmt.Println(Base62Encode(b))
+}
+*/
